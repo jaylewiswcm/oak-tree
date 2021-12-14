a917b0e4eece8918a86923eac460d52af884312c
wcmbristol <benjamin.kenny@worldclassmedia.co.uk>
2021-12-14 10:54:14 +0000

    Updated fonts locally to be used in production

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 import StickyFooter from "../elements/sticky/StickyFooter";
@@ -10,6 +10,75 @@ interface ComponentProps  {
 
  const Layout = ({ children }:ComponentProps) => {
     return (
+        <>
+        <Head>
+            
+        {/* <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaLightItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaBoldItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaBoldRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaBlackItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaSemiboldItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaLightRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaSemiboldRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtraboldRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtraboldItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaBlackRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaThinItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtrathinItalic.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaThinRegular.woff2" as="font" type="font/woff2" crossOrigin='' />
+        <link rel="preload" href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtrathinRegular.woff2" as="font" type="font/woff2" crossOrigin='' /> */}
+        {/* <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaSemiboldRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaLightRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtrathinRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaExtraboldRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaBoldRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          />
+        <link
+            href="/fonts/Averta/FONTSPRINGDEMO-AvertaBlackRegular.woff2"
+            rel="preload"
+            as="font"
+            crossOrigin=""
+          /> */}
+          {/* <link
+            rel="preload"
+            href="/fonts/EBGaramond/EBGaramond-Medium.ttf"
+            as="font"
+            crossOrigin=""
+          /> */}
+        </Head>
+       
         <div className='main'>
             <Header />
             {/* <BreadCrumbs /> */}
@@ -17,6 +86,7 @@ interface ComponentProps  {
             <StickyFooter />
             <Footer />
         </div>
+        </>
     )
 }
 
