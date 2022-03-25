0ed17ad25587da881b3f2fea9295406db08d5058
wcmbristol <benjamin.kenny@worldclassmedia.co.uk>
2022-03-25 16:11:30 +0000

    Deployment Testing - Chair Collection page mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { NextSeo } from 'next-seo';
 // Component
+import Testimonials from '../components/sections/testimonials/Testimonials';
 import HomeCollections from '../components/sections/collections/HomeCollections';
 import FooterCollections from '../components/sections/collections/FooterCollections';
 import Reviews from '../components/sections/reviews/Reviews';
-import Testimonials from '../components/sections/testimonials/Testimonials';
 
 type MyProps = { };
 type MyState = { requestClassName: string, uspOverlay:string };
