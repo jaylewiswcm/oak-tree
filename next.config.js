d0b2be7951d733d1aa86c760be9b304bfaad0062
jaylewiswcm <jay.lewis@worldclassmedia.co.uk>
2022-05-11 16:23:54 +0100

    Adding env vars to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  future: { webpack5: true }
+  future: { webpack5: true },
+  env: { 
+      BUSINESS_UNIT_ID: process.env.BUSINESS_UNIT_ID 
+    }
 }
 
