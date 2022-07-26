be6bc008945ead4afec6aa3f173eb26922b0ed4b
jaylewiswcm <jay.lewis@worldclassmedia.co.uk>
2022-07-26 14:09:01 +0100

    Sitemap dynamic routes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/sitemap.xml',
-        destination: '/api/sitemap',
-      },
-    ]
-},
   reactStrictMode: true,
   future: { webpack5: true },
   experiments: {
