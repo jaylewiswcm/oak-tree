014903c304ecc7fd7e8b351eea94a39351d23a37
jaylewiswcm <jay.lewis@worldclassmedia.co.uk>
2022-07-26 14:34:30 +0100

    Sitemap errors fixed

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,16 +1,50 @@
 import React from 'react';
+import * as fs from 'fs'
 
 import { getAllProducts } from '../lib/shopify';
 
 const BASE_URL = 'https://dev.oaktreemobility.co.uk';
 
+const staticPaths = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "api",
+        "_app.ts",
+        "_app.tsx",
+        "_document.ts",
+        "_document.tsx",
+        "404.tsx",
+        "sitemap.xml.ts",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${BASE_URL}/${staticPagePath}`;
+    });
+
+
 const createSitemap = (products:any) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${staticPaths
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url.split('.tsx').join('')}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
         ${products
           .map((product:any) => {
             return `
                 <url>
                     <loc>${`${BASE_URL}/${product.type}/the-${product.name}`}</loc>
+                    <lastmod>${new Date().toISOString()}</lastmod>
+                    <changefreq>monthly</changefreq>
+                    <priority>1.0</priority>
                 </url>
             `;
           })
